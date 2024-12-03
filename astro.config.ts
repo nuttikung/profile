8bbda3f3206dda22e6e95b6dace232c064c4ceb7
Phudit Chuengjaroen <phudit@greenmoons.co.th>
2024-12-03 16:59:04 +0700

    add slash

--- astro.config.ts
+++ astro.config.ts
@@ -9,7 +9,7 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  base: 'profile',
+  base: '/profile',
   integrations: [
     tailwind({
       applyBaseStyles: false,
